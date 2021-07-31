1983f4931f5f78a62635e34572154566500ecb8e
ErickTiznado <88002820+ErickTiznado@users.noreply.github.com>
2021-07-31 16:14:48 -0600

    Update Program.cs

--- Grupo1_calculadoradeConsola/Program.cs
+++ Grupo1_calculadoradeConsola/Program.cs
@@ -7,8 +7,8 @@ namespace Calculator
         static void Main(string[] args)
         {
             //Erick Mauricio Tiznado 
-            //Nathaly Milena Zelaya
-//Franklin Stanley Larin
+            //Franklin Stanley Larin
+            
             // Declara Variables y luego inicializa a cero.
             Double valor1 = 0; Double valor2 = 0;
 
